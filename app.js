f9da1e243e67ca8e54cab016bfb239c8a15b6b70
fikyair <shiming_xsm@163.com>
2019-04-02 15:27:05 +0800

    整理代码

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
-const fs = require('fs');
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const router = require('koa-router')();
 const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+//导入controller middleware:
+const controller = require('./controller')
 
-const router = require('koa-router')();
-let files = fs.readdirSync(__dirname + '/controllers');
-
-app.use(bodyParser());
-
-//过滤出s文件
-let js_files = files.filter((f)=>{
-    return f.endsWith('.js');
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
 });
+// parse request body:
+app.use(bodyParser());
+// add controllers 使用middleware:
+app.use(controller());
+app.listen(3000);
+console.log('app started at port 3000...');
 
-//处理每个js文件:
-for (let f of js_files) {
-    console.log(`process controller: ${f}...`);
-    //导入js文件：
-    let mapping = require(__dirname + '/controllers/' + f);
-    console.log('router',router)
-    for (let url in mapping) {
-        if(url.startsWith("GET")){
-            //如果url类似“GET XXX”：
-            let path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if(url.startsWith('POST')) {
-            //如果url类似“POST XXX”:
-            let path = url.substring(5);
-            router.post(post, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            //无效的url
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
+// let files = fs.readdirSync(__dirname + '/controllers');
+// //过滤出s文件
+// let js_files = files.filter((f)=>{
+//     return f.endsWith('.js');
+// });
 
+// // 处理每个js文件:
+// for (var f of js_files) {
+//     console.log(`process controller: ${f}...`);
+//     // 导入js文件:
+//     let mapping = require(__dirname + '/controllers/' + f);
+//     for (var url in mapping) {
+//         if (url.startsWith('GET ')) {
+//             // 如果url类似"GET xxx":
+//             var path = url.substring(4);
+//             router.get(path, mapping[url]);
+//             console.log(`register URL mapping: GET ${path}`);
+//         } else if (url.startsWith('POST ')) {
+//             // 如果url类似"POST xxx":
+//             var path = url.substring(5);
+//             router.post(path, mapping[url]);
+//             console.log(`register URL mapping: POST ${path}`);
+//         } else {
+//             // 无效的URL:
+//             console.log(`invalid URL: ${url}`);
+//         }
+//     }
+// }
+//---------
 
 
 
--- controllers/index.js
+++ controllers/index.js
@@ -11,8 +11,8 @@ let fn_index = async (ctx, next) => {
 let fn_signin = async(ctx, next)=>{
     let
        name = ctx.request.body.name || '',
-       pssword = ctx.request.body.password || '';
-       console.log(`signin with name: ${name}, password: ${pssword}`);
+       password = ctx.request.body.password || '';
+       console.log(`signin with name: ${name}, password: ${password}`);
         if(name === 'koa' && password === '12345') {
             ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
         }else{
