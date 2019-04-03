4760cf04191347ba7bcedd45f290eca5890e7cd2
fikyair <shiming_xsm@163.com>
2019-04-03 11:30:25 +0800

    修复静态文件bug

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const Koa = require('koa');
-const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 //导入controller middleware:
-const controller = require('./controller')
+const controller = require('./controller');
+const templating = require('./templating');
+const app = new Koa();
+const isProduction = process.env.NODE_ENV === 'production';
 
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
     await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
+
+// static file support:
+if (! isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
 // parse request body:
 app.use(bodyParser());
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
 // add controllers 使用middleware:
 app.use(controller());
 app.listen(3000);
+
 console.log('app started at port 3000...');
 
 // let files = fs.readdirSync(__dirname + '/controllers');
